54165a817a22109768280cd1c6356809caa20f3a
izadora Montenegro <izadoraoamontenegro@gmail.com>
2024-06-11 12:33:39 -0300

    Initial Commit

---
+++ Cloudies/CloudiesApp.swift
@@ -0,0 +1,18 @@
+//
+//  CloudiesApp.swift
+//  Cloudies
+//
+//  Created by Izadora de Oliveira Albuquerque Montenegro on 11/06/24.
+//
+
+import SwiftUI
+import GoogleGenerativeAI
+
+@main
+struct CloudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
