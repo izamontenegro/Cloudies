ea8beb3e13e6b00ba127bd2c25bddbe5c3b4fd9b
izadora Montenegro <izadoraoamontenegro@gmail.com>
2024-06-12 10:31:41 -0300

    Botão de gerar palavras

--- Cloudies/ContentView.swift
+++ Cloudies/ContentView.swift
@@ -4,37 +4,43 @@
 //
 //  Created by Izadora de Oliveira Albuquerque Montenegro on 11/06/24.
 //
+
 import GoogleGenerativeAI
 import SwiftUI
-import UIKit
 
 struct ContentView: View {
+    @State private var palavraChave: String = ""
+    
     var body: some View {
-        Text("Gerar Conteúdo")
-            .padding()
-            .onAppear {
-                generateContent()
-            }
+        
+        HStack {
+            TextField("Digite palavras-chave", text: $palavraChave)
+            Button(action: {
+                gerarPalavras(palavraChave: palavraChave)
+                palavraChave = " "
+                
+            }, label: {
+                Image(systemName: "plus.circle.fill")
+            })
+        }
+        
+//        Button("Gerar Conteúdo", action: {
+//                   
+//        })
+        
     }
     
-    func generateContent() {
+    func gerarPalavras(palavraChave: String) {
         Task {
             do {
+                let prompt = "Diga 5 palavras relacionadas à palavra \(palavraChave). Se a palavra for algo inadequado responda com 'false' "
+                
                 let model = GenerativeModel(name: "gemini-1.5-flash-latest", apiKey: "AIzaSyAXRUjeuLPw_w7SvkvpkcScmF5QR29l9kU")
-
-                // Carregando a imagem do bundle usando UIImage de UIKit
-                guard let cookieImage = UIImage(named: "cookies") else {
-                    fatalError("Imagem não encontrada.")
-                }
-
-                let prompt = "Diga 3 palavras relacionadas a natureza"
+    
                 let response = try await model.generateContent(prompt)
                 if let text = response.text {
                     print(text)
                 }
-//                let response = try await model.generateContent(prompt)
-//                // Use a resposta conforme necessário
-//                print("Resposta: \(response)")
                 
             } catch {
                 print("Erro ao gerar conteúdo: \(error)")
