1e4592ca67b6dcbda0a936a177486b492dc7dc1b
izadora Montenegro <izadoraoamontenegro@gmail.com>
2024-06-12 10:58:34 -0300

    Paasando palavras geradas pela IA para a tela

--- Cloudies/ContentView.swift
+++ Cloudies/ContentView.swift
@@ -10,44 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var palavraChave: String = ""
+    @State private var geradas: String = ""
     
     var body: some View {
-        
-        HStack {
-            TextField("Digite palavras-chave", text: $palavraChave)
-            Button(action: {
-                gerarPalavras(palavraChave: palavraChave)
-                palavraChave = " "
-                
-            }, label: {
-                Image(systemName: "plus.circle.fill")
-            })
+        VStack{
+            HStack {
+                TextField("Digite palavras-chave", text: $palavraChave)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button(action: {
+                    Task {
+                        geradas = await gerarPalavras(palavraChave: palavraChave)
+                        palavraChave = " "
+                        
+                    }
+                }, label: {
+                    Image(systemName: "plus.circle.fill")
+                })
+            }
+            
+            Text(geradas)
         }
-        
-//        Button("Gerar Conteúdo", action: {
-//                   
-//        })
-        
     }
     
-    func gerarPalavras(palavraChave: String) {
-        Task {
+    func gerarPalavras(palavraChave: String) async -> String {
+        
             do {
-                let prompt = "Diga 5 palavras relacionadas à palavra \(palavraChave). Se a palavra for algo inadequado responda com 'false' "
+                let prompt = "Diga 5 palavras relacionadas à palavra \(palavraChave). Se a palavra for algo inapropriada (sexual ou violento) responda com 'false'. Se for algo como 'racismo', 'machismo' e etc, forneça palavras que incentivem o combate dessas praticas"
                 
                 let model = GenerativeModel(name: "gemini-1.5-flash-latest", apiKey: "AIzaSyAXRUjeuLPw_w7SvkvpkcScmF5QR29l9kU")
     
                 let response = try await model.generateContent(prompt)
                 if let text = response.text {
-                    print(text)
+                    return text
                 }
-                
+                else {
+                    return "kkk"
+                }
             } catch {
                 print("Erro ao gerar conteúdo: \(error)")
+                return "kkk"
             }
             
             
-        }
+        
     }
 }
 
