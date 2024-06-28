35a1ee7a45eea7a9aef70d68d723f20f0c49e5ee
nhagss <yagosouzaramos1@gmail.com>
2024-06-27 21:27:36 -0300

    add: Adicionado o botão para adicionar problemas a mão

---
+++ Cloudies/Screens/SheetAdicionarTextos.swift
@@ -0,0 +1,18 @@
+//
+//  sheetAdicionarTextos.swift
+//  Cloudies
+//
+//  Created by Yago Souza Ramos on 27/06/24.
+//
+
+import SwiftUI
+
+struct sheetAdicionarTextos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sheetAdicionarTextos()
+}
