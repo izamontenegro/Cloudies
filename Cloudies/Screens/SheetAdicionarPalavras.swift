114670583637dba4c0ac11e768f2a0cd4bd252ba
izadora Montenegro <izadoraoamontenegro@gmail.com>
2024-06-28 11:20:05 -0300

    Design das telas de adicionar ideia e outros ajustes

--- Cloudies/Screens/SheetAdicionarPalavras.swift
+++ Cloudies/Screens/SheetAdicionarPalavras.swift
@@ -13,26 +13,74 @@ struct SheetAdicionarPalavras: View {
     @Binding var linhaGeracao: [LinhaDePalavras]
     @State private var palavras: [Palavra] = [Palavra(texto: ""), Palavra(texto: ""), Palavra(texto: ""), Palavra(texto: ""), Palavra(texto: "")]
     var body: some View {
-        VStack {
-            ForEach($palavras.indices, id: \.self) { i in
-                TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $palavras[i].texto)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    Image("imagemBrainstorm")
+
+                    VStack {
+                        ForEach($palavras.indices, id: \.self) { i in
+                            Text("Ideia \(i+1)")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.leading, -178)
+                                .padding(.bottom, -5)
+                            TextField("Digite sua ideia...", text: $palavras[i].texto)
+                                .padding()
+                                .frame(width: 357, height: 39)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.black, lineWidth: 1)
+                                        .frame(width: 357, height: 39)
+                                )
+                        }
+                    }
+                   
+                    Button(action: {
+                        palavras.removeAll { palavra in
+                            palavra.texto == ""
+                        }
+                        
+                        for i in palavras.indices {
+                            palavras[i].isFromUser = true
+                        }
+                        
+                        linhaGeracao.append(LinhaDePalavras(palavras: palavras))
+                        
+                        dismiss()
+                        
+                    }, label: {
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 357, height: 63)
+                                .foregroundStyle(.AZUL)
+                                .shadow(radius: 4)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.black, lineWidth: 1)
+                                        .frame(width: 357, height: 63)
+                                )
+                            
+                            Text("Adicionar")
+                                .font(.title2)
+                        }
+                    })
+                    .padding(.top, 33)
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+            .defaultScrollAnchor(.bottom)
+            .navigationTitle("Adicionar")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Voltar") {
+                        dismiss()
+                    }
+                    
+                }
             }
         }
-        Button("Enviar") {
-            palavras.removeAll { palavra in
-                palavra.texto == ""
-            }
-            
-            for i in palavras.indices {
-                palavras[i].isFromUser = true
-            }
-            
-            linhaGeracao.append(LinhaDePalavras(palavras: palavras))
-            
-            dismiss()
-        }
-        .font(.title)
-        .padding()
-        .background(.black)
     }
 }
